05b3a1afd5c35e608be490bdaf0e67ea0642d6e6
iTz_Nao <63752443+iTzNao@users.noreply.github.com>
2024-04-12 13:48:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-def calculate_ttk(self, health, damage, magazine_size, rpm, reload_speed):
+from math import ceil
+
+def calculate_ttk(health, damage, magazine_size, rpm, reload_speed):
     if damage > health:
         ttk = 0
         return ttk
 
-    rounds_needed = math.ceil((health / damage) - 1)
-    reloads_needed = math.ceil(rounds_needed / magazine_size) - 1
+    rounds_needed = ceil((health / damage) - 1)
+    reloads_needed = ceil(rounds_needed / magazine_size) - 1
 
     if magazine_size == None or reloads_needed == 0:
         ttk = round(rounds_needed / (rpm / 60), 2)
